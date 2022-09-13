7cdc5193e1ab4f14e0a56632cc7934900570ab96
Omoseke <omosekeslami@gmail.com>
2022-09-13 05:11:56 -0700

    third commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: result
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: parameter
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int lastDigit = n % 10;
+
+	if (lastDigit < 0)
+		lastDigit *= -1;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
