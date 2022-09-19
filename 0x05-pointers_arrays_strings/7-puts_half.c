d00699c49a981374e7986a827689290cf6384f87
Omoseke <omosekeslami@gmail.com>
2022-09-19 09:28:04 -0700

    betty fix on 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 
 /**
  * puts_half - prints half of a string
- * @str - string
+ * @str: string
  *
  * Return: nothing
  */
@@ -11,7 +11,9 @@ void puts_half(char *str)
 	int i;
 	int n;
 
-	for (n = 0; str[n] != '\0'; n++);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+
 	if (n % 2 != 0)
 	{
 		for (i = (n - 1) / 2; str[i] != '\0'; i++)
