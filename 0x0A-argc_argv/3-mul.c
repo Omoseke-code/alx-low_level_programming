f12f4825eff1e69c03301dd5e888708f8f3e5cff
Omoseke <omosekeslami@gmail.com>
2022-09-30 04:25:03 -0700

    fix on 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
