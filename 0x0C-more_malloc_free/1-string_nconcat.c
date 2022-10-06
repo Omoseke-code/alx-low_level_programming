d98f0662ce32ad68ea0621f11f99e44039966453
Omoseke <omosekeslami@gmail.com>
2022-10-06 08:36:39 -0700

    executed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	if (n > len2)
+	if (n > j)
 		n = j;
-	s = malloc(sizeof(i + n + 1);
+	s = malloc((i + n) + 1);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
