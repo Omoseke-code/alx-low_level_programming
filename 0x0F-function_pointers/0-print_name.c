5dd03b33e27325a1eed833cf4dd17387af4ad6b3
Omoseke <omosekeslami@gmail.com>
2022-10-12 14:00:53 -0700

    mandatory

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: pointer to function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
