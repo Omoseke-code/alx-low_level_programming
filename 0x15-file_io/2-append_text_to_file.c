180c978eb68683aa91d37c7cb4faf4b663d1d24a
Omoseke Salami <omosekesalami@gmail.com>
2022-10-31 11:58:47 -0700

    Write a function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,14 +17,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if(text_content == NULL)
-		return (-1);
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-	rwr = write(fd, text_content, nletters);
-	if (rwr == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(fd, text_content, nletters);
+		if (rwr == -1)
+			return (-1);
+	}
 
 	close(fd);
 
