849cc586d2b219fa13dd1f6947bdf9469df7d92c
Omoseke Salami <omosekesalami@gmail.com>
2022-12-08 11:51:51 -0800

    added comment for return value

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,6 +4,7 @@
  * add_dnodeint - adds a new node at the beginning of the list
  * @head: head pointer
  * @n: element to be added
+ * Return: a new list
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
