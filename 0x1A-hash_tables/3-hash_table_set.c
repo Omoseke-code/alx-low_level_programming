5c5f2ee379d3b7d502611a0740c5649d51b7ba3b
Omoseke Salami <omosekesalami@gmail.com>
2023-01-07 02:57:58 -0800

    fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (entry == NULL)
 	{
 		ht->array[slot] = ht_pair(key, value);
-		return (1);
+		return (0);
 	}
 
 	while (entry != NULL)
